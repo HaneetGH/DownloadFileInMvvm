9d6be0e9460e4423cac7b91d135f40b91642849b
haneetsinghchhabra <HOTh@n!!t52p93>
2021-07-10 13:08:15 +0530

    complete

--- app/src/androidTest/java/com/haneet/assignment/ui/MainActivityTest.kt
+++ app/src/androidTest/java/com/haneet/assignment/ui/MainActivityTest.kt
@@ -30,6 +30,7 @@ class MainActivityTest {
 
     @Test
     fun A_isViewsVisible() {
+
         Espresso.onView(ViewMatchers.withId(R.id.url))
             .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
         Espresso.onView(ViewMatchers.withId(R.id.btn))
@@ -37,8 +38,10 @@ class MainActivityTest {
 
     }
 
+
     @Test
     fun B_start() {
+        Espresso.onView(ViewMatchers.withId(R.id.url)).perform(ViewActions.clearText())
         Espresso.onView(ViewMatchers.withId(R.id.url))
             .perform(ViewActions.typeText("https://www.eurofound.europa.eu/sites/default/files/ef_publication/field_ef_document/ef1710en.pdf"));
         Espresso.onView(ViewMatchers.withId(R.id.btn))
